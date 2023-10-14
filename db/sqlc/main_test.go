8fdc513a7806ae280fcbbcceddc5f5fbe79255b3
h <>
2023-10-14 13:40:24 +0300

    add endpoints

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -5,21 +5,20 @@ import (
 	_ "github.com/lib/pq"
 	"log"
 	"os"
+	"simplebank/util"
 	"testing"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("can not load config:", err)
+	}
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("can not connect to db:", err)
 	}
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"simplebank/api"
+	db "simplebank/db/sqlc"
+	"simplebank/util"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("can not load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("can not connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("can not start server:", err)
+	}
+}
